fc82413739778073a2b0c02c363df8eb67066e23
Dhiraj Kumar <dhirajkr032@gmail.com>
2022-11-25 00:20:10 +0530

    add constructor in ErrorResponse

--- src/main/java/com/app/wecare/dto/response/ErrorResponse.java
+++ src/main/java/com/app/wecare/dto/response/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.app.wecare.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ErrorResponse extends GenericResponse{
     private String errorMessage;
 }
