0804c320357a7ef6e69be4ad6912785a8027143f
Dhiraj Kumar <dhirajkr032@gmail.com>
2022-11-27 23:16:40 +0530

    enhance exception handling

--- src/main/java/com/app/wecare/exception/WecareExceptionHandler.java
+++ src/main/java/com/app/wecare/exception/WecareExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,8 +22,16 @@ public class WecareExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(WecareException.class)
+    @ExceptionHandler(WecareException.class )
     public ResponseEntity<ErrorMessage> handleWeCareException(WecareException err){
         return new ResponseEntity<>(err.getErrorMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ServletRequestBindingException.class)
+    public ResponseEntity<ErrorMessage> handleServletRequestBindingException(ServletRequestBindingException err){
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage(err.getMessage());
+        errorMessage.setCode(400);
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
