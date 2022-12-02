3f87f0097d083de0facbec79d80b833e15645f61
Dhiraj Kumar <dhirajkr032@gmail.com>
2022-12-03 00:14:28 +0530

    add loginDTO

---
+++ src/main/java/com/app/wecare/dto/request/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.app.wecare.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+    private Long id;
+    private String password;
+}
