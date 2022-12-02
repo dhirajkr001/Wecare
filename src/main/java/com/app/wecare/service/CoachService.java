b764d86a2670d29ce4cb5784f17c307958342efb
Dhiraj Kumar <dhirajkr032@gmail.com>
2022-12-03 00:16:38 +0530

    update for loginCoach

--- src/main/java/com/app/wecare/service/CoachService.java
+++ src/main/java/com/app/wecare/service/CoachService.java
@@ -1,8 +1,13 @@
 package com.app.wecare.service;
 
+import com.app.wecare.dto.request.LoginDTO;
 import com.app.wecare.entity.Coach;
 import com.app.wecare.exception.WecareException;
 
 public interface CoachService {
-    public String addCoach(Coach coach);
+     String addCoach(Coach coach);
+
+     Boolean loginCoach(LoginDTO loginDTO);
+
+     Coach fetchCoachByCoachId(Long coachId) throws WecareException;
 }
--- src/main/java/com/app/wecare/service/CoachServiceImpl.java
+++ src/main/java/com/app/wecare/service/CoachServiceImpl.java
@@ -1,10 +1,13 @@
 package com.app.wecare.service;
 
+import com.app.wecare.dto.request.LoginDTO;
 import com.app.wecare.entity.Coach;
 import com.app.wecare.exception.WecareException;
 import com.app.wecare.repository.CoachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CoachServiceImpl implements CoachService{
@@ -17,4 +20,19 @@ public class CoachServiceImpl implements CoachService{
         coachRepository.save(coach);
         return "Coach Added successfully";
     }
+
+    @Override
+    public Boolean loginCoach(LoginDTO loginDTO) {
+        Optional<Coach> coach = coachRepository.findById(loginDTO.getId());
+        return (coach.isPresent() && coach.get().getPassword().equals(loginDTO.getPassword()));
+    }
+
+    @Override
+    public Coach fetchCoachByCoachId(Long coachId) throws WecareException {
+        Optional<Coach> coach = coachRepository.findById(coachId);
+        if(coach.isEmpty()) throw new WecareException("No coach found for the coachId", 4044);
+        return coach.get();
+    }
+
+
 }
