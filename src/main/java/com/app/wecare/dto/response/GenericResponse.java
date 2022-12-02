095bc952080c8eb3a1eb7fb39b41be8153eca580
Dhiraj Kumar <dhirajkr032@gmail.com>
2022-12-03 00:05:42 +0530

    add data in response

--- src/main/java/com/app/wecare/dto/response/GenericResponse.java
+++ src/main/java/com/app/wecare/dto/response/GenericResponse.java
@@ -1,5 +1,6 @@
 package com.app.wecare.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,10 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GenericResponse {
     private Integer code;
     private String message;
     private UUID correlationId;
+    Object data;
 }
